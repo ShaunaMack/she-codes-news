661cfd94aa806c85cafd0f0e53ff0e677f753e7f
Shauna Mack <shaunakatemack@gmail.com>
2021-08-21 13:01:41 +1000

    add custom user model and form

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    path('add-story/', views.AddStoryView.as_view(), name='newStory')
+    path('', views.IndexView.as_view(), name='index'), # /news/
+    path('<int:pk>/', views.StoryView.as_view(), name='story'), # /news/3
+    path('add-story/', views.AddStoryView.as_view(), name='newStory') # /news/add-story
 ]
+ 
