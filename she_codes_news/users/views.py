b250db8e81a1cae6ffbaebb44a684f4929336573
Shauna Mack <shaunakatemack@gmail.com>
2021-08-27 18:19:05 +1000

    add sign up link to nav and user account page

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -9,3 +9,8 @@ class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+class UserAccountView(generic.DetailView):
+    model = CustomUser
+    template_name = 'users/account.html'
+    context_object_name = 'user'
