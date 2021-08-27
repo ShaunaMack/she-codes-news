91815eaa7abab91b87ebf397d2b619dc9e151bdf
Shauna Mack <shaunakatemack@gmail.com>
2021-08-27 18:55:32 +1000

    register NewsStiry in admin

--- she_codes_news/news/admin.py
+++ she_codes_news/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import NewsStory
 
 # Register your models here.
+class NewsStoryAdmin(admin.ModelAdmin):
+    list_filter = ("author",)
+    list_display = ("title", "author", "pub_date",)
+
+
+admin.site.register(NewsStory, NewsStoryAdmin)
